0775197a5341937302e8bee7bfe7dd856dae92d9
N-Sridhar <nsridharbtech@gmail.com>
2020-10-04 23:41:31 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD1oaDXPMBJzQr3A02hySOVmO6FTQd1S4w",
+    authDomain: "insta-mock-react.firebaseapp.com",
+    databaseURL: "https://insta-mock-react.firebaseio.com",
+    projectId: "insta-mock-react",
+    storageBucket: "insta-mock-react.appspot.com",
+    messagingSenderId: "26527387794",
+    appId: "1:26527387794:web:de7a0368ca1935e25d964b",
+    measurementId: "G-SEWVSXPJ61"
+  };
